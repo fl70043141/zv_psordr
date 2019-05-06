62a335b9944934a548385b6c2214eec7d0ff7a9c
Fahry Lafir <fahrydgt@gmail.com>
2019-05-06 12:01:22 +0530

    flotbt

--- application/views/sales/order_ecatalog/item_grid.php
+++ application/views/sales/order_ecatalog/item_grid.php
@@ -1,7 +1,61 @@
 
  
 <?php // echo '<pre>'; print_r($category_id); die;?>
+<style>
 
+    .label-container{
+            position:fixed;
+            bottom:48px;
+            right:105px;
+            display:table;
+            visibility: hidden;
+    }
+
+    .label-text{
+            color:#FFF;
+            background:rgba(51,51,51,0.5);
+            display:table-cell;
+            vertical-align:middle;
+            padding:10px;
+            border-radius:3px;
+    }
+
+    .label-arrow{
+            display:table-cell;
+            vertical-align:middle;
+            color:#333;
+            opacity:0.5;
+    }
+
+    .float{
+            position:fixed;
+            width:60px;
+            height:60px;
+            bottom:40px;
+            right:40px;
+            background-color:#06C;
+            color:#FFF;
+            border-radius:50px;
+            text-align:center;
+            box-shadow: 2px 2px 3px #999;
+    }
+
+    .my-float{
+            font-size:24px;
+            margin-top:18px;
+    }
+
+    a.float + div.label-container {
+      visibility: hidden;
+      opacity: 0;
+      transition: visibility 0s, opacity 0.5s ease;
+    }
+
+    a.float:hover + div.label-container{
+      visibility: visible;
+      opacity: 1;
+    }
+</style>
 <div class="row">
 <div class="col-md-12">
     <!--<br>-->   
@@ -79,6 +133,15 @@
             
           </div>
        
+              <!--FLOAT BTN-->
+              <a href="<?php echo base_url('Sales_order_items/'.(($order_id=="")?'add':'edit/'.$order_id));?>" class="float">
+                <i class="fa fa-check my-float"></i>
+                </a>
+                <div class="label-container">
+                <div class="label-text">Finalize Order</div>
+                <i class="fa fa-play label-arrow"></i>
+                </div>
+              <!--END FLOAT BTN-->
     </section>              
    
      </div>
