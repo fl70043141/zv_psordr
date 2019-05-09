e5da122ea3de92e349465d6ade75c1405cd22dd5
Fahry Lafir <fahrydgt@gmail.com>
2019-05-09 14:27:07 +0530

    1231Tsd

--- application/views/purchasing_items/manage_purchasing_items.php
+++ application/views/purchasing_items/manage_purchasing_items.php
@@ -439,7 +439,7 @@ $(document).ready(function(){
                                 
                                 var row_str = '<tr style="pad1ding:10px" id="tr_'+rowCount+'">'+ 
                                                         '<td><span id="'+rowCount+'_row_cntr" class="row_counter_cls"></span></td>'+
-                                                        '<td><input hidden name="inv_items['+rowCount+'][item_desc]" value="'+res2.item_name+'"><input hidden name="inv_items['+rowCount+'][cat_id]" value="'+res2.id+'">'+res2.item_name+'</td>'+
+                                                        '<td><input hidden name="inv_items['+rowCount+'][item_desc]" value="'+res2.item_name+'"><input hidden name="inv_items['+rowCount+'][cat_id]" value="'+res2.id+'">'+res2.category_name+'</td>'+
                                                         '<td><input hidden name="inv_items['+rowCount+'][item_code]" class="item_code_row" value="'+$('#item_code').val()+'">'+$('#item_code').val()+'</td>'+
                                                         '<td><input hidden name="inv_items['+rowCount+'][item_desc]" value="'+res2.item_name+'"><input hidden name="inv_items['+rowCount+'][cat_id]" value="'+res2.id+'">'+res2.item_name+'</td>'+
                                                         '<td align="center"><input hidden name="inv_items['+rowCount+'][item_quantity]" value="'+$('#item_quantity').val()+'"><input hidden name="inv_items['+rowCount+'][item_quantity_2]" value="'+(($('#item_quantity_2').val()==null)?0:$('#item_quantity_2').val())+'">'+
